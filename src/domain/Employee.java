a9f44ad8db0ef2ef16a6738fa43d3cf4cafafb87
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-04-16 23:06:48 +0300

    Update Employee.java

--- src/domain/Employee.java
+++ src/domain/Employee.java
@@ -1,68 +1,69 @@
 package domain;
-public class Employee {
 
-    @Override
-    public String toString() {
-        return "\nEmployee ID= " + ID + "\nName= " + name + "\nJobTitle= " + jobTitle + "\nLevel= " + level + "\nDept= " + dept;
-    }
+/**
+ * Базовий клас, що описує загальні характеристики співробітника компанії.
+ * Служить фундаментом для всіх інших посад у системі.
+ * @author Пилипенко
+ */
+public class Employee {
 
     private String name;
     private String jobTitle;
-    private int ID;
     private int level;
     private String dept;
 
-   // private static int employeesCount = 0;
+    @Override
+    public String toString() {
+        return "\nEmployee Info:" + "\nName: " + name + "\nJob Title: " + jobTitle + "\nLevel: " + level + "\nDept: " + dept;
+    }
 
+    /**
+     * Створює співробітника з повним набором даних.
+     * @param name ім'я співробітника
+     * @param jobTitle назва посади
+     * @param level рівень кваліфікації
+     * @param dept відділ
+     */
     public Employee(String name, String jobTitle, int level, String dept) {
-        this();
-        setName(name);
+        this.name = name;
         this.jobTitle = jobTitle;
-        switch (level) {
-            case 1:
-            case 2:
-            case 3:
-                this.level = level;
-                break;
-            default:
-                this.level = 1;
-        }
+        this.level = level;
         this.dept = dept;
     }
 
+    /**
+     * Конструктор за замовчуванням.
+     * Створює об'єкт із базовими значеннями параметрів.
+     */
     public Employee() {
-        ID = (int)(Math.random()*1000);
-//        if (ID > 10000) {
-//            throw new NumberFormatException("ERROR! Employee's ID cannot be greater than 10000. Employee is not created!");
-//        }
-    }
-
-    public void setJobTitle(String job) {
-        jobTitle = job;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
+        this.name = "John Doe";
+        this.jobTitle = "Worker";
+        this.level = 1;
+        this.dept = "Staff";
     }
 
     public String getName() {
         return name;
     }
 
-    public void setLevel(int level) {
-        switch (level) {
-            case 1:
-            case 2:
-            case 3:
-                this.level = level;
-                break;
-            default:
-                this.level = 1;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
     }
 
     public int getLevel() {
         return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 
     public String getDept() {
@@ -71,25 +72,5 @@ public class Employee {
 
     public void setDept(String dept) {
         this.dept = dept;
-    }
-
-    public void setName(String name) {
-//        boolean correctName = true;
-//        for (int i = 0; i < name.length(); i++) {
-//            char s = name.charAt(i);
-//            if (Character.isDigit(s)) {
-//                correctName = false;
-//            }
-//        }
-//        if (correctName) {
-//            this.name = name;
-//        } else {
-//            this.name = "John Doe";
-//        }
-        if (name.matches("^([a-zA-Z]+[\\'\\,\\.\\-]?[a-zA-Z ]*)+[ ]([a-zA-Z]+[\\'\\,\\.\\-]?[a-zA-Z ]+)+$")) {
-            this.name = name;
-        } else {
-            this.name = "John Doe";
-        }
     }
 }
