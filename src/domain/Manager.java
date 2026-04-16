5007db07cb66bee4e8a2725520bc9422d366abe9
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-04-16 23:04:12 +0300

    Update Manager.java

--- src/domain/Manager.java
+++ src/domain/Manager.java
@@ -1,44 +1,61 @@
 package domain;
+
+/**
+ * Представляє співробітника на керівній посаді.
+ * Керує групою інших співробітників.
+ */
 public class Manager extends Employee {
 
+    private Employee[] employees;
+
+    /**
+     * Повний конструктор для створення менеджера.
+     */
     public Manager(Employee[] employees, String name, String jobTitle, int level, String dept) {
         super(name, jobTitle, level, dept);
         this.employees = employees;
     }
 
-    @Override
-    public String toString() {
-        return super.toString()+"\nEmployees: "+getEmployees(); 
-    }
-
+    /**
+     * Конструктор для створення менеджера тільки зі списком підлеглих.
+     */
     public Manager(Employee[] employees) {
         super();
         this.employees = employees;
     }
-    
+
+    /**
+     * Конструктор за замовчуванням.
+     * Створює масив для 10 співробітників.
+     */
     public Manager() {
         super();
         employees = new Employee[10];
     }
 
-    private Employee[] employees;
-
+    /**
+     * Формує список імен усіх підлеглих.
+     * @return рядок з іменами через кому
+     */
     public String getEmployees() {
         String s = "";
         for (Employee e : employees) {
             s = s + e.getName() + ", ";
         }
-        s=s.substring(0, s.length() - 2);
+        s = s.substring(0, s.length() - 2);
         return s;
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + "\nEmployees: " + getEmployees(); 
+    }
+
     public void setEmployees(Employee[] employees) {
-        this.employees=employees;
+        this.employees = employees;
     }
 
     public Employee[] getEmployeesList() {
         return employees;
     }
-
-    
 }
