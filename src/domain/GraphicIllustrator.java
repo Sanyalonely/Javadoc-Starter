3ff4479e42fff8e0ca298cef249e3c7f4a4de34c
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-04-16 23:04:52 +0300

    Update GraphicIllustrator.java

--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,9 @@
 package domain;
+
+/**
+ * Клас для опису ілюстраторів.
+ * На даному етапі розширює клас Artist без додаткових полів.
+ */
 public class GraphicIllustrator extends Artist {
+    // Наслідує всі методи та поля класу Artist
 }
